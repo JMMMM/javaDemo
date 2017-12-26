49f6de58b04a725f142f4cef1c5db51fbeda49ad
wujiaming <wujiaming@iplas.com.cn>
2017-12-26 11:28:15 +0800

    netty study

--- src/main/java/netty/HelloServer.java
+++ src/main/java/netty/HelloServer.java
@@ -1,14 +1,9 @@
 package netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import nio.TimeServerHandler;
-
-import java.nio.channels.SocketChannel;
 
 import static io.netty.channel.ChannelOption.SO_BACKLOG;
 
