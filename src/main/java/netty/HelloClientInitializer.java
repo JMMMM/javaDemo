f1318519926a083ee466215f9678d0db35861c61
wujiaming <wujiaming@iplas.com.cn>
2017-12-26 11:23:39 +0800

    netty study

---
+++ src/main/java/netty/HelloClientInitializer.java
@@ -0,0 +1,18 @@
+package netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class HelloClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()))
+                .addLast("decoder", new StringDecoder())
+                .addLast("encoder", new StringEncoder())
+                .addLast("handler", new HelloClientHandler());
+    }
+}
---
+++ src/main/java/netty/HelloServer.java
@@ -0,0 +1,38 @@
+package netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import nio.TimeServerHandler;
+
+import java.nio.channels.SocketChannel;
+
+import static io.netty.channel.ChannelOption.SO_BACKLOG;
+
+public class HelloServer {
+    private static final int port = 7878;
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(SO_BACKLOG, 1024)
+                    .childHandler(new HelloServerInitializer());
+            bootstrap.bind(port).sync().channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
+
