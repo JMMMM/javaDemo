2de0127cb12faaf0d66011859388dae6f3a3f6b4
wujiaming <wujiaming@iplas.com.cn>
2018-01-11 11:51:15 +0800

    netty study

---
+++ src/main/java/netty/ByteBufDemo.java
@@ -0,0 +1,22 @@
+package netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.Charset;
+
+public class ByteBufDemo {
+    public static void main(String[] args) {
+        Charset utf8 = Charset.forName("UTF-8");
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        ByteBuf sliced = buf.slice(0, 14);
+        System.out.println(sliced.toString(utf8));
+        System.out.println(buf.toString(utf8));
+        System.out.println(buf.readSlice(14).toString(utf8));
+        System.out.println(buf.toString(utf8));
+        System.out.println(buf.slice());
+        buf.setByte(0, (byte) 'J');
+        buf = Unpooled.copiedBuffer("Netty in Action rocks!!", utf8);
+        System.out.println(buf.isWritable());
+    }
+}
