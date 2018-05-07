219e2c9c5ec45503c853738fc5d69856807882d6
wujiaming <wujiaming@iplas.com.cn>
2018-05-07 11:13:15 +0800

    netty study

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,14 +1,30 @@
 package demo;
 
+import com.github.dapeng.core.SoaException;
+import com.isuwang.soa.admin.AdminCacheServiceClient;
+import com.isuwang.soa.company.CompanyOnBinlogServiceClient;
+import com.isuwang.soa.company.service.CompanyOnBinlogService;
+import com.isuwang.soa.crm.company.enums.CrmCompanyType;
+
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class Demo {
-    public static void main(String[] args) {
-        Calendar calender = Calendar.getInstance();
-        calender.setTime(new Date());
-        System.out.println(calender.get(Calendar.DAY_OF_YEAR));
-
+    public static void main(String[] args) throws SoaException {
+//        String[] str = {"广州", "上海", "北京", "四川", "茂名", "佛山", "湖南", "广州快塑", "公司", "常州拓荣塑料有限公司"};
+        String[] str = {"海南华塑新型管业有限公司"};
+        CompanyOnBinlogServiceClient client = new CompanyOnBinlogServiceClient();
+        Random random = new Random();
+        long max = 0;
+        while (true) {
+            int index = random.nextInt(str.length);
+            long start = System.currentTimeMillis();
+            String value = client.findCompanyByKeyword(str[index], CrmCompanyType.PURCHASER);
+            long takes = System.currentTimeMillis() - start;
+            if (takes > max) System.out.println(takes + "ms");
+            System.out.println(value + " " + takes + "ms");
+        }
 
     }
 }
--- src/main/java/netty/HelloServerHandler.java
+++ src/main/java/netty/HelloServerHandler.java
@@ -1,22 +1,22 @@
-package netty;
-
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public class HelloServerHandler extends SimpleChannelInboundHandler<String> {
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-        System.out.println(ctx.channel().remoteAddress() + " Say: " + msg);
-        ctx.writeAndFlush("Received your message !\n");
-    }
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("RemoteAddress:" + ctx.channel().remoteAddress());
-        ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostAddress() + " service!\n");
-        super.channelActive(ctx);
-    }
-}
+//package netty;
+//
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.channel.SimpleChannelInboundHandler;
+//
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
+//
+//public class HelloServerHandler extends SimpleChannelInboundHandler<String> {
+//    @Override
+//    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+//        System.out.println(ctx.channel().remoteAddress() + " Say: " + msg);
+//        ctx.writeAndFlush("Received your message !\n");
+//    }
+//
+//    @Override
+//    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        System.out.println("RemoteAddress:" + ctx.channel().remoteAddress());
+//        ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostAddress() + " service!\n");
+//        super.channelActive(ctx);
+//    }
+//}
