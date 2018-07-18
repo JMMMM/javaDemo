53b4345878acdb076cd86177598f2cdd333335c2
wujiaming <wujiaming@iplas.com.cn>
2018-07-18 12:39:02 +0800

    netty study

--- src/main/java/demo/ThreadPrintDemo3.java
+++ src/main/java/demo/ThreadPrintDemo3.java
@@ -1,35 +1,53 @@
 package demo;
 
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+
 class ThreadPrintDemo3 {
+    volatile int num = 0;
+    boolean flag = false;
 
-    static volatile int num = 0;
-    static volatile boolean flag = false;
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10000; i++) {
+            new ThreadPrintDemo3().job();
+        }
+    }
 
-    public static void main(String[] args) {
+    private void job() throws InterruptedException {
 
+        final List<Integer> result = new Vector<>();
+
+        final CountDownLatch latch = new CountDownLatch(2);
         Thread t1 = new Thread(() -> {
             for (; 100 > num; ) {
                 if (!flag && (num == 0 || ++num % 2 == 0)) {
-
-                    System.out.println(num);
+                    result.add(num);
                     flag = true;
                 }
             }
+            latch.countDown();
         }
         );
 
         Thread t2 = new Thread(() -> {
             for (; 100 > num; ) {
                 if (flag && (++num % 2 != 0)) {
-                    System.out.println(num);
+                    result.add(num);
                     flag = false;
                 }
             }
+            latch.countDown();
         }
         );
 
         t1.start();
         t2.start();
 
+        latch.await();
+        for (int i = 0; i <= 100; i++) {
+            assert result.get(0) == i;
+        }
+        System.out.println("====================done!");
     }
 }
