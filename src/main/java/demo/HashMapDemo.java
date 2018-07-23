d29e1e51b393a325c6d0891b6461b06be4914e51
wujiaming <wujiaming@iplas.com.cn>
2018-07-23 14:43:32 +0800

    comt

--- src/main/java/demo/HashMapDemo.java
+++ src/main/java/demo/HashMapDemo.java
@@ -8,7 +8,6 @@ public class HashMapDemo {
         Map<String,Integer> map = new HashMap<>();
         map.put("数学",2);
         map.put("化学",8);
-        map.put("hello",1);
         System.out.println(map.get("数学"));
         System.out.println(map.get("化学"));
         System.out.println(map.get("hello"));
