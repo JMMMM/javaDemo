b787d50ebfca6e76261d2b9408f84ff4fcc140b7
wujiaming <wujiaming@iplas.com.cn>
2018-07-23 14:12:46 +0800

    comt

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,31 +1,5 @@
 package demo;
 
-import com.github.dapeng.core.SoaException;
-import com.isuwang.soa.admin.StaffServiceClient;
-import com.isuwang.soa.company.ComponentOnBinlogServiceClient;
-import com.isuwang.soa.crm.company.enums.CrmCompanyType;
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.StringUtils;
-import sun.misc.Unsafe;
-
-import javax.sql.DataSource;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.lang.reflect.Field;
-import java.nio.Buffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
 /**
  * 这个代码并不能达到奇偶数轮流输出的效果
  * 仔细阅读代码还是会有可能出现死锁的情况，这种方式时错误的
