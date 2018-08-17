f6876fd54e233d9d20e6e7139dac0859382cbde4
wujiaming <wujiaming@iplas.com.cn>
2018-08-17 16:06:30 +0800

    123

---
+++ src/main/java/demo/IntegerDemo.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        int a = 300;
+        Integer b = 300;
+        System.out.println(a==b);
+        System.out.println(b==a);
+    }
+}
