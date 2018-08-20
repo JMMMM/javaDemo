08e282761a204875df255f2e7df9fc0997b9dca1
wujiaming <wujiaming@iplas.com.cn>
2018-08-20 20:43:25 +0800

    123

--- src/main/java/face/DemoTest6.java
+++ src/main/java/face/DemoTest6.java
@@ -1,7 +1,26 @@
 package face;
 
+import java.util.HashMap;
+
 public class DemoTest6 {
     public static void main(String[] args) {
         System.out.println(Integer.valueOf(30).hashCode());
+        HashMap<Person, String> temp = new HashMap<>();
+        for (int i = 0; i <= 1000; i++) {
+            Person p1 = new Person();
+            temp.put(p1, p1.toString());
+        }
+    }
+}
+
+class Person {
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 16;
     }
 }
