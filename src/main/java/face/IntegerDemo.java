f95af4781dae4a4425ac66b533c4336dfeabe8dc
wujiaming <wujiaming@iplas.com.cn>
2018-08-20 21:31:17 +0800

    123

---
+++ src/main/java/face/IntegerDemo.java
@@ -0,0 +1,11 @@
+package face;
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+        System.out.println(Integer.toBinaryString(-8));
+        System.out.println(Integer.toBinaryString(8));
+    }
+
+}
