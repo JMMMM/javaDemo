57494908f755f14bc47329242613dbdbd02e46f8
wujiaming <wujiaming@iplas.com.cn>
2018-08-23 20:02:47 +0800

    123

---
+++ src/main/java/demo/ThreadPoolExecuteDemo.java
@@ -0,0 +1,28 @@
+package demo;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecuteDemo {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecute = new ThreadPoolExecutor(2, 3, 30, TimeUnit.SECONDS, new ArrayBlockingQueue(100));
+        threadPoolExecute.execute(() -> {
+            while (true) {
+                System.out.println("hello");
+            }
+        });
+        threadPoolExecute.execute(() -> {
+            while (true) {
+                System.out.println("hello2222222");
+            }
+        });
+        threadPoolExecute.shutdown();
+        threadPoolExecute.execute(() -> {
+            while (true) {
+                System.out.println("hello3333333");
+            }
+        });
+        threadPoolExecute.shutdown();
+    }
+}
