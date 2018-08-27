a5a6b7c2ec000045b4aebe6b62b13a3cfae8bc04
wujiaming <wujiaming@iplas.com.cn>
2018-08-27 11:28:52 +0800

    123

--- src/main/java/demo/TestClass.java
+++ src/main/java/demo/TestClass.java
@@ -1,6 +1,16 @@
 package demo;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class TestClass {
     private int m;
     public int inc(){return m + 1;}
+
+    public static void main(String[] args) {
+        double   f   =   111234.5585;
+        BigDecimal   b   =   new BigDecimal(f);
+        double   f1   =   b.setScale(0, RoundingMode.DOWN).doubleValue();
+        System.out.println(f1);
+    }
 }
