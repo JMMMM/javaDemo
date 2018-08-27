cf9ffbe9f2c0ec6fe5b583145817eeb06807629d
wujiaming <wujiaming@iplas.com.cn>
2018-08-27 15:15:16 +0800

    123

--- src/main/java/demo/ThreadPoolExecuteDemo.java
+++ src/main/java/demo/ThreadPoolExecuteDemo.java
@@ -5,11 +5,16 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecuteDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadPoolExecutor threadPoolExecute = new ThreadPoolExecutor(2, 3, 30, TimeUnit.SECONDS, new ArrayBlockingQueue(1));
         threadPoolExecute.execute(() -> {
             while (true) {
                 System.out.println("hello");
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
             }
         });
         threadPoolExecute.execute(() -> {
@@ -23,6 +28,11 @@ public class ThreadPoolExecuteDemo {
                 System.out.println("hello3333333");
             }
         });
-        threadPoolExecute.shutdown();
+        Thread.sleep(3000);
+        /**
+         * 此处shutdownNow并没有实际生效，由于代码中都没有可以interrupted的点
+         * 所以即使shutdownNow依然没法马上结束线程池
+         */
+        threadPoolExecute.shutdownNow();
     }
 }
