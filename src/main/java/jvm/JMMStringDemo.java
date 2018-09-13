78ce30cd5b7851b4ff8bdd7feac547031bd7f635
wujiaming <wujiaming@iplas.com.cn>
2018-09-13 10:34:14 +0800

    push

---
+++ src/main/java/jvm/JMMStringDemo.java
@@ -0,0 +1,23 @@
+package jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 查看String常量池存放在 java heap中
+ * -verbose:gc -Xmx1M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ * OOM java heap space
+ */
+public class JMMStringDemo {
+    static String base = "string";
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String str = base + base;
+            base = str;
+            list.add(str.intern());
+        }
+
+    }
+}
