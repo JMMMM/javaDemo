a1a352627e97b14d1b252373342c9fadab6b8d64
wujiaming <wujiaming@iplas.com.cn>
2018-09-13 18:15:44 +0800

    push

---
+++ src/main/java/kafka/KafkaProducerDemo.java
@@ -0,0 +1,60 @@
+package kafka;
+
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class KafkaProducerDemo {
+
+    private final String topic;
+    private final Boolean isAsync;
+    private final KafkaProducer<Integer, String> prod;
+
+    public KafkaProducerDemo(String topic, Boolean isAsync) {
+        Properties props = new Properties();
+        try {
+            props.load(new FileInputStream("producer.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.prod = new KafkaProducer(props);
+        this.topic = topic;
+        this.isAsync = isAsync;
+    }
+
+
+    private void send(Integer key, String message) {
+        ProducerRecord<Integer, String> record = new ProducerRecord(this.topic, key, message);
+        if (isAsync) {
+            prod.send(record, (recordMetadata, e1) ->
+                    System.out.println("#offset:" + recordMetadata.offset()));
+        } else {
+            try {
+                prod.send(record).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        KafkaProducerDemo producerDemo = new KafkaProducerDemo("kafka_study_demo", false);
+        producerDemo.send(1, "message_1");
+    }
+}
