4c40941883d325a0d46d274b0d6fa71eba2d9124
wujiaming <wujiaming@iplas.com.cn>
2018-11-02 11:31:29 +0800

    push

--- src/main/java/jvm/NotInitialization.java
+++ src/main/java/jvm/NotInitialization.java
@@ -4,25 +4,12 @@ package jvm;
  * 类加载
  * 每次执行只运行一个case
  */
-class SuperClass {
-    static {
-        System.out.println("SuperClass init!");
-    }
-
-    public static int value = 123;
-}
-
-class SubClass extends SuperClass{
-    static {
-        System.out.println("SubClass init");
-    }
-}
 
 public class NotInitialization {
     public static void main(String[] args) {
         //case 1:当子类调用父类属性时，不会初始化子类。
-//        System.out.println(SubClass.value);
+        System.out.println(SubClass.value);
         //case 2:-XX:+TraceClassLoading
-        SuperClass[] sca = new SuperClass[10];
+//        SuperClass[] sca = new SuperClass[10];
     }
 }
