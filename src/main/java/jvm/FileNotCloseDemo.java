c30c58d41bc83a506386de8e7b8177a365764be0
wujiaming <wujiaming@iplas.com.cn>
2018-11-30 10:51:09 +0800

    123

---
+++ src/main/java/jvm/FileNotCloseDemo.java
@@ -0,0 +1,16 @@
+package jvm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileNotCloseDemo {
+    public static void main(String[] args) throws IOException {
+
+        File tempFile = File.createTempFile("test", "ttt");
+        FileInputStream fi = new FileInputStream(tempFile);
+
+        System.in.read();
+    }
+}
