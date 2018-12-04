ea3b7f143aad33489a00c0de2ef0f651af8bc17d
wujiaming <wujiaming@iplas.com.cn>
2018-12-04 16:44:35 +0800

    123

---
+++ src/main/java/demo/Demo1.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Demo1 {
+
+    public static void add(int temp) {
+        temp++;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> a = new HashMap<>();
+        a.put("1", "a");
+        List<Map> b = new ArrayList<Map>();
+        b.add(a);
+        a = null;
+        Map aa = b.get(0);
+        System.out.println(aa.get("1"));
+        //结论传递的是引用的副本
+        //测试值传递
+        int xx = 1;
+        add(xx);
+        System.out.print(xx);
+    }
+}
+
