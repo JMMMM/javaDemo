bc7e8f4d43939233ac31a1d1d38a56def626cd45
wujiaming <wujiaming@iplas.com.cn>
2019-01-28 18:22:49 +0800

    123

--- src/main/java/jvm/ConcurrentHashMapDemo.java
+++ src/main/java/jvm/ConcurrentHashMapDemo.java
@@ -1,11 +1,21 @@
 package jvm;
 
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentHashMapDemo {
     public static void main(String[] args) {
-        ConcurrentHashMap map = new ConcurrentHashMap();
+        Map<String,String> map = new ConcurrentHashMap<String, String>();
+//        Map<String,String> map = new HashMap<String, String>();
 //        map.put(null,"");//NullPointerException
 //        map.put("",null);//NullPointerException
+        List<String> ls = new ArrayList<String>();
+        ls.add("a");
+        ls.add("b");
+        ls.add("c");
+        ls.add("d");
+        for(String key:ls){
+            ls.remove(key);
+        }
     }
 }
