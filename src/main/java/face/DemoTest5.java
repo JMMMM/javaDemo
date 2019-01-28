bf113c07ba0545605cd9c7918d94d495cb193a0f
wujiaming <wujiaming@iplas.com.cn>
2019-01-28 17:54:47 +0800

    123

--- src/main/java/face/DemoTest5.java
+++
@@ -1,7 +0,0 @@
-package face;
-
-public class DemoTest5 {
-    public static void main(String[] args) {
-        System.out.println(30 * 24 * 60 * 60 * 1000L == 30 * 24 * 60 * 60 * 1000);
-    }
-}
---
+++ src/main/java/jvm/ConcurrentHashMapDemo.java
@@ -0,0 +1,11 @@
+package jvm;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+        ConcurrentHashMap map = new ConcurrentHashMap();
+//        map.put(null,"");//NullPointerException
+//        map.put("",null);//NullPointerException
+    }
+}
