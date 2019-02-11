1e6c94bbbf9cd2bb4ae6199c7f4ffd5a5a3502fc
wujiaming <wujiaming@iplas.com.cn>
2019-02-11 10:40:45 +0800

    123

--- src/main/java/demo/Demo4.java
+++ src/main/java/demo/Demo4.java
@@ -3,8 +3,16 @@ package demo;
 import java.time.LocalDateTime;
 
 public class Demo4 {
+    static Demo4Test test = new Demo4Test();
+
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now().isAfter(LocalDateTime.of(2019, 2, 14, 23, 59, 59)));
 
+    }
+}
+
+class Demo4Test {
+    public Demo4Test() {
+        System.out.println("demo4Test init ");
     }
 }
---
+++ src/main/java/demo/Demo5.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo5 {
+    static Demo4 test = new Demo4();
+
+    public static void main(String[] args) {
+        System.out.println("demo 5 ");
+    }
+}
