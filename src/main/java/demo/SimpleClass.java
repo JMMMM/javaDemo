e8a1673a19ad97506c94b41aa7e5ca6588532e58
wujiaming <wujiaming@iplas.com.cn>
2019-02-12 10:18:07 +0800

    123

---
+++ src/main/java/demo/SimpleClass.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * 原文地址
+ * http://blog.jamesdbloom.com/JVMInternals.html
+ */
+public class SimpleClass {
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
