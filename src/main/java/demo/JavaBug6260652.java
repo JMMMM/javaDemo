905d2bb63844ccb4eab300f38cf64a6923a73399
wujiaming <wujiaming@iplas.com.cn>
2019-02-15 16:29:50 +0800

    123

---
+++ src/main/java/demo/JavaBug6260652.java
@@ -0,0 +1,26 @@
+package demo;
+
+import jvm.SubClass;
+import jvm.SuperClass;
+import sun.jvm.hotspot.debugger.cdbg.BaseClass;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaBug6260652 {
+
+    public static void test1() {
+        List<String> list = Arrays.asList("abc");
+        System.out.println(list.getClass());
+
+        Object[] objArray = list.toArray();
+        System.out.println(objArray.getClass());
+        objArray[0] = new Object();
+    }
+
+    public static void main(String[] args) {
+        String[] strArray = new String[10];
+        Object[] objArray = strArray;
+        objArray[0] = new Object();
+    }
+}
--- src/main/java/demo/ListDemo.java
+++ src/main/java/demo/ListDemo.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 
 /**
  * filter和foreach同时进行时，filter和foreach交替执行，只有当filter为true时，才回去执行foreach
- *
  */
 public class ListDemo {
     private static int j = 0;
@@ -19,10 +18,27 @@ public class ListDemo {
         //6
         System.out.println(j);
 
-        j=0;
-        ls.stream().filter((t)->{j++;return true;}).forEach((t)->j++);
+        j = 0;
+        ls.stream().filter((t) -> {
+            j++;
+            return true;
+        }).forEach((t) -> j++);
         //10
         System.out.println(j);
+
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        l1.add(4);
+        l1.add(5);
+//        for (Integer t : l1) {
+//            l1.remove(t);
+//        }
+
+        Integer[] intArray = new Integer[]{6,6,6,6,6,6,6,6};
+        l1.toArray(intArray);
+        System.out.println(Arrays.toString(intArray));
     }
 
 }
