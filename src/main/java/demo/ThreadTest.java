9033319965d2b5b413770859b3bfda8115738407
wujiaming <wujiaming@iplas.com.cn>
2019-02-20 09:55:53 +0800

    123

---
+++ src/main/java/demo/ThreadTest.java
@@ -0,0 +1,42 @@
+package demo;
+
+/**
+ * 用于测试类加载过程中，只允许一个线程对类进行初始化，其余的线程必须等待
+ * 失败的。
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> new ThreadTestObj());
+        t1.setName("thread1");
+
+        Thread t2 = new Thread(() -> new ThreadTestObj());
+        t2.setName("thread2");
+
+        t1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.start();
+    }
+
+}
+
+
+class ThreadTestObj {
+    private static int age = 15;
+
+    public ThreadTestObj() {
+        System.out.println(Thread.currentThread().getName() + "ThreadTestObj start ......");
+        while (true) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//                break;
+//            }
+        }
+//        System.out.println(Thread.currentThread().getName() + "ThreadTestObj end ......");
+    }
+}
