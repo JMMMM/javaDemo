a71e32e4fe9bbbc78d45a922ef307454ef812d6c
wujiaming <wujiaming@iplas.com.cn>
2019-03-04 09:45:30 +0800

    123

--- src/main/java/demo/Demo3.java
+++ src/main/java/demo/Demo3.java
@@ -1,15 +1,18 @@
 package demo;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 
 public class Demo3 {
     public static void main(String[] args) throws InterruptedException {
         Object obj = new Object();
         WeakReference wr = new WeakReference(obj);
         System.out.println(wr.get());
-        obj=null;
-        Thread.sleep(5*1000);
+        obj = null;
+        Thread.sleep(5 * 1000);
         System.out.println(wr.get());
-
+        HashMap map = new HashMap<String, Integer>();
+        System.out.println(map.put("1", 1));
+        System.out.println(map.put("1", 2));
     }
 }
