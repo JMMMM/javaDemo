0a5410e5d09acba95031272c877803955628bb22
wujiaming <wujiaming@iplas.com.cn>
2019-05-07 18:33:20 +0800

    123

--- src/main/java/leetCode/Demo5.java
+++ src/main/java/leetCode/Demo5.java
@@ -8,7 +8,9 @@ public class Demo5 {
         if (s == null || s.length() < 1) return "";
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
+            //aba
             int len1 = expandAroundCenter(s, i, i);
+            //abba
             int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
             if (len > end - start) {
