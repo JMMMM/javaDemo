d59fe9afd0cc96df4095115408c6ee5eeec69e70
wujiaming <wujiaming@iplas.com.cn>
2019-05-08 10:12:09 +0800

    123

---
+++ src/main/java/demo/StaticTest.java
@@ -0,0 +1,32 @@
+package demo;
+
+/**
+ * 加载->验证->准备->解析->初始化->使用->卸载
+ */
+public class StaticTest {
+    public static void main(String[] args) {
+        staticFunction();
+    }
+
+    static StaticTest st = new StaticTest();
+
+    static {
+        System.out.println("1");
+    }
+
+    {
+        System.out.println("2");
+    }
+
+    StaticTest() {
+        System.out.println("3");
+        System.out.println("a=" + a + ",b=" + b);
+    }
+
+    public static void staticFunction() {
+        System.out.println("4");
+    }
+
+    int a = 110;
+    static int b = 112;
+}
