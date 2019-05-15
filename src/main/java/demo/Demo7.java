f0d58d6092aed9f417467f402a1555750fa093d4
wujiaming <wujiaming@iplas.com.cn>
2019-05-15 15:52:26 +0800

    123

---
+++ src/main/java/demo/Demo7.java
@@ -0,0 +1,28 @@
+package demo;
+
+/**
+ * demo
+ *
+ * @author jimmy
+ * @date 2019-05-15
+ */
+public class Demo7 {
+    public static void main(String[] args) {
+        System.out.println(Demo7Test.test);
+    }
+}
+
+class Demo7Test {
+    static Demo7Test test = new Demo7Test();
+
+    static {
+        System.out.println("static");
+    }
+
+    static int a = 10;
+
+    {
+        System.out.println("normal");
+        System.out.println(a);
+    }
+}
