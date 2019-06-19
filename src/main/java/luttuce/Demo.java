9366182d36e0f24454c30c7f009795e7e91cfa6e
wujiaming <wujiaming@iplas.com.cn>
2019-06-19 14:51:59 +0800

    123

---
+++ src/main/java/luttuce/Demo.java
@@ -0,0 +1,22 @@
+package luttuce;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+/**
+ * luttuce
+ *
+ * @author jimmy
+ * @date 2019-06-04
+ */
+public class Demo {
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create("redis://localhost:6379/0");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> syncCommands = connection.sync();
+        syncCommands.set("key", "Hello,Redis!");
+        connection.close();
+        redisClient.shutdown();
+    }
+}
